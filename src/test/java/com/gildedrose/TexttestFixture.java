51f6131f7b37716edaf4a1159f1926556e0a0d2e
Antonio Di Ferdinando <antonio.di-ferdinando@ext.ec.europa.eu>
2019-08-20 16:34:26 +0200

    Added goldmaster. Amended TextTest fixture to comply to goldmaster.

--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -1,9 +1,32 @@
 package com.gildedrose;
 
-public class TexttestFixture {
-    public static void main(String[] args) {
-        System.out.println("OMGHAI!");
+import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static java.lang.String.format;
+import static org.junit.Assert.assertEquals;
+
+public class TexttestFixture {
+    
+    @Test
+    public void checkGoldMasterCompliance() throws IOException {
+    
+        String goldMasterResultString = new String(Files.readAllBytes(Paths.get("src/test/resources/goldMaster.txt")));
+        
+        String currentResultString = buildCurrentResult().toString();
+        
+        assertEquals(goldMasterResultString, currentResultString);
+        
+    }
+    
+    private static StringBuilder buildCurrentResult() {
+    
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("OMGHAI!\n");
+        
         Item[] items = new Item[] {
                 new Item("+5 Dexterity Vest", 10, 20), //
                 new Item("Aged Brie", 2, 0), //
@@ -15,23 +38,23 @@ public class TexttestFixture {
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
                 // this conjured item does not work properly yet
                 new Item("Conjured Mana Cake", 3, 6) };
-
-        GildedRose app = new GildedRose(items);
-
-        int days = 2;
-        if (args.length > 0) {
-            days = Integer.parseInt(args[0]) + 1;
-        }
-
-        for (int i = 0; i < days; i++) {
-            System.out.println("-------- day " + i + " --------");
-            System.out.println("name, sellIn, quality");
+        
+        
+        GildedRose gildedRose = new GildedRose(items);
+        
+        for(int i = 0; i < 31; i++) {
+            stringBuilder.append(format("-------- day %s --------%n", i));
+            stringBuilder.append(format("%s %s %s%n", "name,", "sellIn,", "quality"));
             for (Item item : items) {
-                System.out.println(item);
+                stringBuilder.append(format("%s, %s, %s%n", item.name, item.sellIn, item.quality));
             }
-            System.out.println();
-            app.updateQuality();
+            if(i != 30) {
+                stringBuilder.append(format("%n"));
+            }
+            
+            gildedRose.updateQuality();
         }
+        
+        return stringBuilder;
     }
-
 }
